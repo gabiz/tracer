fc5c45d410ada46554ef5fc1c2100f5c68347972
Gabi Zuniga <gabiz@gapix.com>
2017-08-22 00:33:40 -0700

    Added matcher module to generate tracing match specifications

---
+++ lib/matcher.ex
@@ -0,0 +1,344 @@
+defmodule ETrace.Matcher do
+  @moduledoc """
+  Matcher translate an elixir expression to a tracing matchspec
+
+  This module is based on ericmj's https://github.com/ericmj/ex2ms
+  module for ets matchspecs.
+  Head logic has been rewritten to accept function like headers instead of
+  tuples.
+  Body logic has been adapted to support tracing commands
+  """
+
+  @bool_functions [
+  :is_atom, :is_float, :is_integer, :is_list, :is_number, :is_pid, :is_port,
+  :is_reference, :is_tuple, :is_binary, :is_function, :is_record, :and, :or,
+  :not, :xor]
+
+  @guard_functions @bool_functions ++ [
+    :abs, :element, :hd, :length, :node, :round, :size, :tl, :trunc, :+, :-, :*,
+    :div, :rem, :band, :bor, :bxor, :bnot, :bsl, :bsr, :>, :>=, :<, :<=, :===,
+    :==, :!==, :!=, :self]
+
+  @body_custom_functions [
+    :count, :gauge, :histogram]
+
+  @body_trace_functions [
+    process_dump: 0, caller: 0, return_trace: 0, excpetion_trace: 0,
+    self: 0, node: 0, disable_trace: [2, 3], enable_trace: [2, 3],
+    get_tcw: 0, set_tcw: 1, get_seq_token: 0, set_seq_token: 2,
+    is_seq_token: 0, trace: 2, silent: 1]
+
+  @elixir_erlang [
+    ===: :"=:=", !==: :"=/=", !=: :"/=", <=: :"=<", and: :andalso, or: :orelse]
+
+  Enum.map(@guard_functions, fn(atom) ->
+    defp is_guard_function(unquote(atom)), do: true
+  end)
+  defp is_guard_function(_), do: false
+
+  Enum.map(@body_custom_functions, fn(atom) ->
+    defp is_custom_function(unquote(atom)), do: true
+  end)
+  defp is_custom_function(_), do: false
+
+  Enum.map(@body_trace_functions, fn({atom, arity}) ->
+    defp is_trace_function(unquote(atom)), do: true
+    defp trace_function_arity?(unquote(atom), val) do
+      if is_list(unquote(arity)) do
+        Enum.member?(unquote(arity), val)
+      else
+        unquote(arity) == val
+      end
+    end
+  end)
+  defp is_trace_function(_), do: false
+
+  Enum.map(@elixir_erlang, fn({elixir, erlang}) ->
+    defp map_elixir_erlang(unquote(elixir)), do: unquote(erlang)
+  end)
+  defp map_elixir_erlang(atom), do: atom
+
+  defmacro match([do: clauses]) do
+    IO.puts "Clauses = #{inspect clauses}"
+    outer_vars = __CALLER__.vars
+    clauses
+    |> Enum.map(fn({:->, _, clause}) ->
+                  translate_clause(clause, outer_vars)
+                end)
+    |> Macro.escape(unquote: true)
+  end
+
+  defmacro match(_) do
+    raise ArgumentError, message: "invalid args to matchspec"
+  end
+
+  defmacrop is_literal(term) do
+    quote do
+      is_atom(unquote(term)) or
+      is_number(unquote(term)) or
+      is_binary(unquote(term))
+    end
+  end
+
+  defp translate_clause([head, body], outer_vars) do
+    IO.puts "Before Head = #{inspect head}"
+    IO.puts "Before Body = #{inspect body} outer_vars = #{inspect outer_vars}"
+    {head, conds, state} = translate_head(head, outer_vars)
+
+    body = translate_body(body, state)
+    {head, conds, body}
+  end
+
+  defp set_annotation(state) do
+    Map.put(state, :annotation, true)
+  end
+
+  defp annotating?(state) do
+    Map.get(state, :annotation, false)
+  end
+
+  defp increase_depth(state) do
+    Map.put(state, :depth, get_depth(state) + 1)
+  end
+
+  defp get_depth(state) do
+    Map.get(state, :depth, 0)
+  end
+
+  # Translate Body
+
+  defp translate_body({:__block__, _, exprs}, state) when is_list(exprs) do
+    body = Enum.map(exprs, &translate_body_term(&1, state))
+    if many_messages?(body) do
+      raise ArgumentError, message: "multiple messages or incompatible actions"
+    else
+      body
+    end
+  end
+
+  defp translate_body(expr, state) do
+     [translate_body_term(expr, state)]
+  end
+
+  defp many_messages?(body) do
+    Enum.reduce(body, 0, fn
+      {:message, _}, acc -> acc + 1
+      _, acc -> acc
+    end) > 1
+  end
+
+  defp translate_body_term({var, _, nil}, state) when is_atom(var) do
+    if match_var = state.vars[var] do
+      if annotating?(state), do: {var, :"#{match_var}"}, else: :"#{match_var}"
+    else
+      raise ArgumentError, message: "variable `#{var}` is unbound in matchspec"
+    end
+  end
+
+  defp translate_body_term({left, right}, state),
+    do: translate_body_term({:{}, [], [left, right]}, state)
+  defp translate_body_term({:{}, _, list}, state) when is_list(list) do
+    list
+    |> Enum.map(&translate_body_term(&1, increase_depth(state)))
+    |> List.to_tuple
+  end
+
+  defp translate_body_term({:%{}, _, list}, state) do
+    list
+    |> Enum.reduce({%{}, state}, fn {key, value}, {map, state} ->
+      value = translate_body_term(value, increase_depth(state))
+      {Map.put(map, key, value), state}
+    end)
+    |> elem(0)
+  end
+
+  defp translate_body_term({:^, _, [var]}, _state) do
+    {:unquote, [], [var]}
+  end
+
+  defp translate_body_term({fun, _, args}, state)
+      when fun === :message or fun === :display and is_list(args) do
+    if get_depth(state) == 0 do
+      match_args = Enum.map(args,
+                            &translate_body_term(&1,
+                                                state
+                                                |> set_annotation()
+                                                |> increase_depth()
+                                                ))
+      {fun, match_args}
+    else
+      raise ArgumentError, message: "`#{fun}` cannot be nested"
+    end
+  end
+
+  defp translate_body_term({fun, _, args}, state)
+      when is_atom(fun) and is_list(args) do
+    cond do
+      is_guard_function(fun) ->
+        match_args = Enum.map(args,
+                              &translate_body_term(&1, increase_depth(state)))
+        match_fun = map_elixir_erlang(fun)
+        [match_fun | match_args] |> List.to_tuple
+      is_custom_function(fun) ->
+        if get_depth(state) == 0 do
+          match_args = Enum.map(args,
+                                &translate_body_term(&1,
+                                                    state
+                                                    |> set_annotation()
+                                                    |> increase_depth()
+                                                    ))
+          match_fun = map_elixir_erlang(fun)
+          {:message, [{:_cmd, match_fun} | match_args]}
+        else
+          raise ArgumentError, message: "`#{fun}` cannot be nested"
+        end
+      is_trace_function(fun) ->
+        if trace_function_arity?(fun, length(args)) do
+          match_args = Enum.map(args,
+                                &translate_body_term(&1, increase_depth(state)))
+          match_fun = map_elixir_erlang(fun)
+          [match_fun | match_args] |> List.to_tuple
+        else
+          raise ArgumentError,
+                message: "`#{fun}/#{length(args)}` is not recognized"
+        end
+      true ->
+        raise ArgumentError, message: "`#{fun}` is not recognized"
+    end
+  end
+
+  defp translate_body_term(list, state) when is_list(list) do
+    Enum.map(list, &translate_body_term(&1, state))
+  end
+
+  defp translate_body_term(literal, _state) when is_literal(literal) do
+    literal
+  end
+
+  defp translate_body_term(_, _state), do: raise_expression_error()
+
+  # Translate Condition
+  defp translate_cond({var, _, nil}, state) when is_atom(var) do
+    if match_var = state.vars[var] do
+      :"#{match_var}"
+    else
+      raise ArgumentError, message: "variable `#{var}` is unbound in matchspec"
+    end
+  end
+
+  defp translate_cond({left, right}, state),
+    do: translate_cond({:{}, [], [left, right]}, state)
+  defp translate_cond({:{}, _, list}, state) when is_list(list) do
+    list
+    |> Enum.map(&translate_cond(&1, state))
+    |> List.to_tuple
+  end
+
+  defp translate_cond({:^, _, [var]}, _state) do
+    {:unquote, [], [var]}
+  end
+
+  defp translate_cond({fun, _, args}, state)
+      when is_atom(fun) and is_list(args) do
+    if is_guard_function(fun) do
+      match_args = Enum.map(args, &translate_cond(&1, state))
+      match_fun = map_elixir_erlang(fun)
+      [match_fun | match_args] |> List.to_tuple
+    else
+      raise ArgumentError, message: "`#{fun}` is not allowed in condition"
+    end
+  end
+
+  defp translate_cond(list, state) when is_list(list) do
+    Enum.map(list, &translate_cond(&1, state))
+  end
+
+  defp translate_cond(literal, _state) when is_literal(literal) do
+    literal
+  end
+
+  defp translate_cond(_, _state), do: raise_expression_error()
+
+  defp translate_head([{:when, _, params}], outer_vars)
+      when is_list(params) and length(params) > 1 do
+    {param, condition} = Enum.split(params, -1)
+
+    # {head, state} = translate_param(param, outer_vars)
+    initial_state = %{vars: [], count: 0, outer_vars: outer_vars}
+    {head, state} = do_translate_param(param, initial_state)
+
+    condition = translate_cond(condition, state)
+    {head, condition, state}
+  end
+
+  defp translate_head([], outer_vars) do
+    {[], [], %{vars: [], count: 0, outer_vars: outer_vars}}
+  end
+  defp translate_head(param, outer_vars) when is_list(param) do
+
+    initial_state = %{vars: [], count: 0, outer_vars: outer_vars}
+    {head, state} = do_translate_param(param, initial_state)
+
+    # {head, state} = translate_param(param, outer_vars)
+    # IO.puts "head = #{inspect head } state = #{inspect state}"
+    {head, [], state}
+  end
+
+  defp translate_head(_, _), do: raise_parameter_error()
+
+  defp do_translate_param({:_, _, nil}, state) do
+    {:_, state}
+  end
+
+  defp do_translate_param({var, _, nil}, state) when is_atom(var) do
+    if match_var = state.vars[var] do
+      {:"#{match_var}", state}
+    else
+      match_var = "$#{state.count+1}"
+      state = state
+        |> Map.update!(:vars, &[{var, match_var} | &1])
+        |> Map.update!(:count, &(&1 + 1))
+      {:"#{match_var}", state}
+    end
+  end
+
+  defp do_translate_param({left, right}, state) do
+    do_translate_param({:{}, [], [left, right]}, state)
+  end
+
+  defp do_translate_param({:{}, _, list}, state) when is_list(list) do
+    {list, state} = Enum.map_reduce(list, state, &do_translate_param(&1, &2))
+    {List.to_tuple(list), state}
+  end
+
+  defp do_translate_param({:^, _, [var]}, state) do
+    {{:unquote, [], [var]}, state}
+  end
+
+  defp do_translate_param(list, state) when is_list(list) do
+    Enum.map_reduce(list, state, &do_translate_param(&1, &2))
+  end
+
+  defp do_translate_param(literal, state) when is_literal(literal) do
+    {literal, state}
+  end
+
+  defp do_translate_param({:%{}, _, list}, state) do
+    Enum.reduce list, {%{}, state}, fn {key, value}, {map, state} ->
+      {key, key_state} = do_translate_param(key, state)
+      {value, value_state} = do_translate_param(value, key_state)
+      {Map.put(map, key, value), value_state}
+    end
+  end
+
+  defp do_translate_param(_, _state), do: raise_parameter_error()
+
+  defp raise_expression_error do
+    raise ArgumentError, message: "illegal expression in matchspec"
+  end
+
+  defp raise_parameter_error do
+    raise ArgumentError, message: "invalid parameters"
+  end
+
+end
---
+++ test/matcher_test.exs
@@ -0,0 +1,82 @@
+defmodule ETrace.Matcher.Test do
+  use ExUnit.Case
+
+  import ETrace.Matcher
+
+  test "no params" do
+    assert (match do  -> :x end) ==
+           [{ [], [], [:x] }]
+  end
+
+  test "basic" do
+    assert (match do x -> x end) ==
+           [{ [:"$1"], [], [:"$1"] }]
+  end
+
+  test "gproc" do
+    assert (match do {{:n, :l, {:client, id}}, pid, _} -> {id, pid} end) ==
+           [{[{{:n, :l, {:client, :"$1"}}, :"$2", :_}], [], [{:"$1", :"$2"}]}]
+  end
+
+  test "match supports bound variables" do
+    id = 5
+    assert (match do {{:n, :l, {:client, ^id}}, pid, _} -> pid end) ==
+           [{[{{:n, :l, {:client, 5}}, :"$1", :_}], [], [:"$1"]}]
+  end
+
+  test "gproc with 3 vars" do
+    assert (match do {{:n, :l, {:client, id}}, pid, third} -> {id, pid, third} end) ==
+           [{[{{:n, :l, {:client, :"$1"}}, :"$2", :"$3"}], [], [{:"$1", :"$2", :"$3"}]}]
+  end
+
+  test "gproc with 1 var and 2 bound vars" do
+    one = 11
+    two = 22
+    assert (match do {{:n, :l, {:client, ^one}}, pid, ^two} -> {^one, pid} end) ==
+           [{[{{:n, :l, {:client, 11}}, :"$1", 22}], [], [{11, :"$1"}]}]
+  end
+
+  test "cond" do
+    assert (match do x when true -> 0 end) ==
+           [{[:"$1"], [true], [0] }]
+
+    assert (match do x when true and false -> 0 end) ==
+           [{[:"$1"], [{ :andalso, true, false }], [0] }]
+  end
+
+  test "multiple matchs" do
+    ms = match do
+      x -> 0
+      y -> y
+    end
+    assert ms == [{[:"$1"], [], [0] }, {[:"$1"], [], [:"$1"] }]
+  end
+
+  test "multiple exprs in body" do
+    ms = match do x ->
+      x
+      0
+    end
+    assert ms == [{[:"$1"], [], [:"$1", 0] }]
+  end
+
+  test "body with message including literals" do
+    ms = match do -> message(:a, :b, :c) end
+    assert ms == [{[], [], [{:message, [:a, :b, :c]}] }]
+  end
+
+  test "body with message including bindings" do
+    ms = match do (a, b, c) -> message(a, b, c) end
+    assert ms == [{[:"$1", :"$2", :"$3"],
+                  [],
+                  [{:message, [a: :"$1", b: :"$2", c: :"$3"]}] }]
+  end
+
+  test "body with count including bindings" do
+    ms = match do (a, b, c) -> count(a, b, c) end
+    assert ms == [{[:"$1", :"$2", :"$3"],
+                  [],
+                  [{:message, [_cmd: :count, a: :"$1", b: :"$2", c: :"$3"]}] }]
+  end
+
+end
