f4855de6e957a5414e83c4e0eaca8cb014c960ed
Gabi Zuniga <gabiz@gapix.com>
2017-08-24 14:49:47 -0700

    Added PidHandler to handle trace events

---
+++ lib/pid_handler.ex
@@ -0,0 +1,79 @@
+defmodule ETrace.PidHandler do
+  @moduledoc """
+  PidHandler is the process consumer handler of trace events
+  It passes them to the event_callback once received
+  """
+  alias __MODULE__
+
+  @default_max_message_count  1000
+  @default_max_message_queue_size 1000
+
+  defstruct message_count: 0,
+            max_message_count: @default_max_message_count,
+            max_message_queue_size: @default_max_message_queue_size,
+            event_callback: nil
+
+  def start(opts) when is_list(opts) do
+    initial_state =
+    Enum.reduce(opts, %PidHandler{}, fn ({keyword, value}, pid_handler) ->
+      Map.put(pid_handler, keyword, value)
+    end)
+
+    if initial_state.event_callback == nil do
+      raise ArgumentError, message: "missing event_callback configuration"
+    end
+    spawn_link(fn -> process_loop(initial_state) end)
+  end
+
+  def stop(pid) when is_pid(pid) do
+      send pid, :stop
+  end
+
+  defp process_loop(state) do
+    check_message_queue_size(state)
+    receive do
+      :stop -> exit(:normal)
+      trace_event when is_tuple(trace_event) ->
+        state
+        |> handle_trace_event(trace_event)
+        |> process_loop()
+      _ignored -> process_loop(state)
+    end
+  end
+
+  defp handle_trace_event(state, trace_event) do
+    case Tuple.to_list(trace_event) do
+      [trace | _] when trace === :trace or trace === :trace_ts ->
+        state
+        |> call_event_callback(trace_event)
+        |> check_message_count()
+      _unknown -> state  # ignore
+    end
+  end
+
+  defp call_event_callback(state, trace_event) do
+    case state.event_callback.(trace_event) do
+      :ok -> state
+      error -> exit(error)
+    end
+  end
+
+  defp check_message_queue_size(%PidHandler{max_message_queue_size: max}) do
+    case :erlang.process_info(self(), :message_queue_len) do
+      {:message_queue_len, len} when len > max ->
+         exit({:message_queue_size, len})
+      _ -> :ok
+    end
+  end
+
+  defp check_message_count(state) do
+    state
+    |> Map.put(:message_count, state.message_count + 1)
+    |> case do
+      %PidHandler{message_count: count, max_message_count: max}
+        when count >= max -> exit(:max_message_count)
+      state -> state
+    end
+  end
+
+end
---
+++ test/pid_handler_test.exs
@@ -0,0 +1,86 @@
+defmodule ETrace.PidHandler.Test do
+  use ExUnit.Case
+
+  alias ETrace.PidHandler
+
+  test "start raises an error when no callback is passed" do
+    assert_raise ArgumentError, "missing event_callback configuration", fn ->
+      PidHandler.start(max_message_count: 1)
+    end
+  end
+
+  test "start spawn a process and returns its pid" do
+    pid = PidHandler.start(event_callback: fn _event -> :ok end)
+    assert is_pid(pid)
+    assert Process.alive?(pid)
+  end
+
+  test "stop causes the process to end with a normal status" do
+    Process.flag(:trap_exit, true)
+    pid = PidHandler.start(event_callback: fn _event -> :ok end)
+    assert Process.alive?(pid)
+    PidHandler.stop(pid)
+    assert_receive({:EXIT, ^pid, :normal})
+    refute Process.alive?(pid)
+  end
+
+  test "max_message_count triggers when too many events are received" do
+    Process.flag(:trap_exit, true)
+    pid = PidHandler.start(max_message_count: 2,
+                           event_callback: fn _event -> :ok end)
+    assert Process.alive?(pid)
+    send pid, {:trace, :foo}
+    send pid, {:trace_ts, :bar}
+    assert_receive({:EXIT, ^pid, :max_message_count})
+    refute Process.alive?(pid)
+  end
+
+  test "unrecognized messages are discarded to avoid queue from filling up" do
+    Process.flag(:trap_exit, true)
+    pid = PidHandler.start(max_message_count: 1,
+                           event_callback: fn _event -> :ok end)
+    assert Process.alive?(pid)
+    for i <- 1..100, do: send pid, {:not_expeted_message, i}
+    case Process.info(self(), :message_queue_len) do
+      {:message_queue_len, len} -> assert len == 0
+      error -> assert error
+    end
+  end
+
+  test "callback is invoked when a trace event is received" do
+    Process.flag(:trap_exit, true)
+    test_pid = self()
+    pid = PidHandler.start(event_callback: fn event ->
+       send test_pid, event
+       :ok
+    end)
+    assert Process.alive?(pid)
+    for i <- 1..100, do: send pid, {:trace, i}
+    for i <- 1..100, do: assert_receive {:trace, ^i}
+  end
+
+  test "process exits if callback does not return :ok" do
+    Process.flag(:trap_exit, true)
+    pid = PidHandler.start(event_callback: fn _event -> :not_ok end)
+    assert Process.alive?(pid)
+    send pid, {:trace, :foo}
+    assert_receive({:EXIT, ^pid, :not_ok})
+    refute Process.alive?(pid)
+  end
+
+  test "process exits if too many messages wait in mailbox" do
+    Process.flag(:trap_exit, true)
+    pid = PidHandler.start(max_message_queue_size: 10,
+                           event_callback: fn _event ->
+                              :timer.sleep(10);
+                              :ok end)
+    assert Process.alive?(pid)
+    for i <- 1..11, do: send pid, {:trace, i}
+    case Process.info(pid, :message_queue_len) do
+      {:message_queue_len, len} -> assert len >= 10
+      error -> assert error
+    end
+    assert_receive({:EXIT, ^pid, {:message_queue_size, 11}})
+    refute Process.alive?(pid)
+  end
+end
